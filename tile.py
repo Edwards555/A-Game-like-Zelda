0cb352426c0520fc5fbc8919ffd6cfe43bc00cf2
Edwards555 <2064565454@qq.com>
2025-01-21 16:55:23 +0800

    砖块更改

--- tile.py
+++ tile.py
@@ -2,9 +2,7 @@ import pygame
 from settings import *
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, pos, groups, sprite_type, surface = pygame.Surface((TILESIZE, TILESIZE))):
+    def __init__(self, pos, groups):
         super().__init__(groups)
-        self.sprite_type = sprite_type
-        self.image = surface
-        if sprite_type == 'object':
-            self.rect = self.image.get_rect(topleft = ())
+        self.image = pygame.image.load('').convert_alpha()
+        self.rect = self.image.get_rect(topleft = pos)
